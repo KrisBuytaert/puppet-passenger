ed72d47233602aae2806cdd423888638c33e88d0
Jan Vansteenkiste <jan@vstone.eu>
2012-01-11 17:07:33 +0000

    Moved to autoload module layout

--- manifests/ssl.pp
+++ manifests/ssl.pp
@@ -1,23 +1,16 @@
 class passenger::ssl {
-  #package { [ "mod_ssl" , "mod_authz_ldap" ]:
-  package { 'mod_ssl':	
-    ensure  => present, 
-    require => Package['httpd'], 
-    notify  => Exec["restart-httpd"]
+  package { [ "mod_ssl" , "mod_authz_ldap" ]:	
+    ensure => present, require => Package["webserver"], 
+    notify => Service["$webserver"]
   } 
-
   file {
-    "/etc/httpd/conf.d/ssl.conf":
-        source  => "puppet:///modules/passenger/ssl.conf",
-        mode    => 0644, owner => root, group => root,
-        require => Package['mod_ssl'],
-	notify  => Exec["restart-httpd"];
+    "/etc/$webserver/conf.d/ssl.conf":
+      content => template('passenger/ssl.conf.erb'),
+      mode => 0644, owner => root, group => root,
+      notify => Exec["reload-apache2"];
     ["/var/cache/mod_ssl", "/var/cache/mod_ssl/scache"]:
-        ensure  => directory,
-        owner   => apache, 
-        group   => root, 
-        mode    => 0750,
-	require => Package['httpd'],
-        notify  => Exec['restart-httpd'];
+      ensure => directory,
+      owner => apache, group => root, mode => 0750,
+      notify => Service["$webserver"];
   }
 }
